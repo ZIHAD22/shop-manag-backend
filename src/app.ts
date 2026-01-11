1dded2810415bce71e393184e491cf306d6b7b97
zihad22 <mdzihad2244@gmail.com>
2026-01-11 18:40:27 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import prisma from "./config/db";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const connectToDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log("DB Connected successfully");
+  } catch (e) {
+    console.log("DB connection Fails");
+  }
+};
+
+connectToDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Sample route
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello, World!");
+});
+
+export default app;
