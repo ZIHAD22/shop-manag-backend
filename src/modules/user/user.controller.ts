0018c342ec1dce5e65031c2cb4a8258330b37c97
zihad22 <mdzihad2244@gmail.com>
2026-02-04 23:45:41 +0600

    add password encryption

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,10 +2,13 @@ import { Request, Response } from "express";
 import { shopOwnerServices } from "./user.services";
 import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
+import bcrypt from "bcryptjs";
 
 export const createShopOwner = catchAsync(
   async (req: Request, res: Response) => {
-    console.log(req.body);
+    const salt = await bcrypt.genSalt(10);
+    req.body.password = await bcrypt.hash(req.body.password, salt);
+
     const createShopOwnerData = await shopOwnerServices.createShopOwner(
       req.body,
     );
